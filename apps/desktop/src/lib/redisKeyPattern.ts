10327722416c5f4673a350c3572c89643bcadbec
t8y2 <1156263951@qq.com>
2026-06-10 11:16:29 +0800

    fix(redis): escape asterisk in fuzzy key search pattern

--- apps/desktop/src/lib/redisKeyPattern.ts
+++ apps/desktop/src/lib/redisKeyPattern.ts
@@ -1,5 +1,5 @@
 const REDIS_GLOB_SPECIAL_CHARS = /[\\*?[\]]/g;
-const REDIS_GLOB_SPECIAL_CHARS_FUZZY = /[\\?[\]]/g;
+const REDIS_GLOB_SPECIAL_CHARS_FUZZY = /[\\*?[\]]/g;
 
 export function escapeRedisGlobText(value: string, fuzzy = false): string {
   return value.replace(fuzzy ? REDIS_GLOB_SPECIAL_CHARS_FUZZY : REDIS_GLOB_SPECIAL_CHARS, "\\$&");
